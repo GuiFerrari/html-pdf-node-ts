2031e9206afddedb4c65ff0c2fb998cba93d3983
Guilherme Ferrari <guiferrari10@hotmail.com>
2021-01-16 20:56:32 -0300

    Added function to convert multiple files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,107 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { PDFOptions } from 'puppeteer';
+import { Promise as PromiseBluebird } from 'bluebird';
+import hb from 'handlebars';
 
-export function generatePdf(file, options, callback) {
-  return a + b;
+type CallBackType = (pdf: any) => void;
+
+interface OptionsProps extends PDFOptions {
+  args?: string[];
 }
 
-console.log('teste: ', generatePdf(1, 2));
+interface FileWithUrl {
+  url: string;
+  content?: never;
+}
+
+interface FileWithContent {
+  url?: never;
+  content: string;
+}
+
+type FileType = FileWithUrl | FileWithContent;
+
+export async function generatePdf(file: FileType, options?: OptionsProps, callback?: CallBackType) {
+  let args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ];
+
+  if(options?.args) {
+    args = options.args;
+    delete options.args;
+  }
+
+  const browser = await puppeteer.launch({
+    args: args
+  });
+
+  const page = await browser.newPage();
+
+  if(file.content) {
+    const template = hb.compile(file.content, { strict: true });
+    const result = template(file.content);
+    const html = result;
+
+    await page.setContent(html);
+  } else {
+    await page.goto(file.url as string, {
+      waitUntil: 'networkidle0',
+    });
+  }
+
+  if(file.content) {}
+
+  return PromiseBluebird.props(page.pdf(options))
+    .then(async function(data) {
+       await browser.close();
+
+       return Buffer.from(Object.values(data));
+    }).asCallback(callback);
+}
+
+async function generatePdfs(files: FileType[], options?: OptionsProps, callback?: CallBackType) {
+  let args = [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ];
+
+  if(options?.args) {
+    args = options.args;
+    delete options.args;
+  }
+
+  const browser = await puppeteer.launch({
+    args: args
+  });
+
+  let pdfs = [];
+
+  const page = await browser.newPage();
+
+  for(let file of files) {
+    if(file.content) {
+      const template = hb.compile(file.content, { strict: true });
+      const result = template(file.content);
+      const html = result;
+
+      await page.setContent(html);
+    } else {
+      await page.goto(file.url as string, {
+        waitUntil: 'networkidle0',
+      });
+    }
+    let pdfObj = JSON.parse(JSON.stringify(file));
+    delete pdfObj['content'];
+    pdfObj['buffer'] = Buffer.from(Object.values(await page.pdf(options)));
+    pdfs.push(pdfObj);
+  }
+
+  return PromiseBluebird.resolve(pdfs)
+    .then(async function(data) {
+       await browser.close();
+       return data;
+    }).asCallback(callback);
+}
+
+module.exports.generatePdf = generatePdf;
+module.exports.generatePdfs = generatePdfs;
